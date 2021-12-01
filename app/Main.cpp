e4a6cbfd95f075dfac99f879486f4b461901f605
Alexander <sssadakov@yandex.ru>
2021-12-01 12:58:42 +0300

    Исправление запуска без параметров

--- app/Main.cpp
+++ app/Main.cpp
@@ -17,37 +17,40 @@ CodeGenerator codeGenerator;
 
 int main(int argc, char* argv[])
 {
-	fs::path filePath = argv[1];
-	std::ifstream ifstream(filePath);
-	bool isContinue = ifstream.is_open();
-	if (!isContinue)
+	if (argc > 1)
 	{
-		std::cout << "failed to open \n";
-	}
-	else
-	{
-		try
+		fs::path filePath = argv[1];
+		std::ifstream ifstream(filePath);
+		bool isContinue = ifstream.is_open();
+		if (!isContinue)
 		{
-			lexical.parse(ifstream);
-			
-			syntax.setLexemeTable(std::move(lexical.getLexemeTable()));
-			syntax.setConstTable(std::move(lexical.getConstTable()));
-			syntax.setIdentTable(std::move(lexical.getIdentTable()));
-
-			syntax.parse();
-
-			auto lexemeTable = std::move(syntax.getLexemeTable());
-			auto programAst = std::move(syntax.getProgramAst());
-
-			for (int i = 0 ; i < programAst.size(); i++)
+			std::cout << "failed to open \n";
+		}
+		else
+		{
+			try
 			{
-				codeGenerator.visit(*programAst[i]);
-				std::cout << "\n";
-			}
+				lexical.parse(ifstream);
 
-		} catch (ParseException &exception)
-		{
-			std::cerr << exception.getError();
+				syntax.setLexemeTable(std::move(lexical.getLexemeTable()));
+				syntax.setConstTable(std::move(lexical.getConstTable()));
+				syntax.setIdentTable(std::move(lexical.getIdentTable()));
+
+				syntax.parse();
+
+				auto lexemeTable = std::move(syntax.getLexemeTable());
+				auto programAst = std::move(syntax.getProgramAst());
+
+				for (int i = 0; i < programAst.size(); i++)
+				{
+					codeGenerator.visit(*programAst[i]);
+					std::cout << "\n";
+				}
+
+			} catch (ParseException& exception)
+			{
+				std::cerr << exception.getError();
+			}
 		}
 	}
 
