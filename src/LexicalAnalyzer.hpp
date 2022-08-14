9ff80ba978deca131ed0cd966f1258079cd7a2a6
Alexander <sssadakov@yandex.com>
2022-08-14 11:34:22 +0300

    LexicalAnalyzer: added printResult

--- src/LexicalAnalyzer.hpp
+++ src/LexicalAnalyzer.hpp
@@ -1,8 +1,10 @@
 #ifndef SRC__LEXICAL_ANALYZER__HPP
 #define SRC__LEXICAL_ANALYZER__HPP
 
+#include <iostream>
 #include <memory>
 #include <sstream>
+#include <string>
 
 #include "ParseException.hpp"
 #include "Util.hpp"
@@ -23,6 +25,8 @@ public:
 
   void parse(std::string &line);
 
+  inline void printResult();
+
 private:
   size_t m_lineCount;
 
@@ -31,7 +35,44 @@ private:
   std::unique_ptr<IdentTable> m_identTable;
 
   void getTokens(std::string &line);
+
+  inline std::string getTokenAttr(Token token);
+
 };
+
+
+/*
+[pos] <tokenType>: attr
+*/
+
+inline void LexicalAnalyzer::printResult() {
+  for (Token token : *m_tokenTable) {
+    std::cout << "[" << token.getTextPosition().first << ", "
+     << token.getTextPosition().second << "] "
+     << token.toString() << getTokenAttr(token)
+     << std::endl;
+  }
+}
+
+inline std::string LexicalAnalyzer::getTokenAttr(Token token) {
+  std::string result = "";
+
+  switch (token.getToken()) {
+  case TokenType::IDENTIFIER:
+    result = ": " + (*m_identTable)[token.getAttribute()];
+    break;
+  case TokenType::NUMBER:{
+    auto attr = (*m_constTable)[token.getAttribute()];
+    result = ": " + std::to_string(attr);
+    break;
+  }
+  default:
+    break;
+  }
+
+  return result;
+}
+
 } // namespace thl
 
 #endif // SRC__LEXICAL_ANALYZER__HPP
